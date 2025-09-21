d3014bf82dbfa69e987f060a76990783478afaa7
gbdettogni <gustavobdettogni@gmail.com>
2025-09-21 15:31:03 -0300

    next.config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
-import bundleAnalyzer from '@next/bundle-analyzer';
-
 /**
  * @type {import('next').NextConfig}
  */
-
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-
-export default withBundleAnalyzer({
-  reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-});
-
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
